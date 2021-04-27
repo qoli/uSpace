ae793d65fa20c4ba1deec628ef38bf3dc62e9116
Ronnie W <llqoli@gmail.com>
2021-04-27 13:06:49 +0800

    InternetAddressType en1

--- lib/utils/address.dart
+++ lib/utils/address.dart
@@ -5,13 +5,13 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 Future<String?> getLocalIpAddress(int port) async {
   String? ipAddress;
 
-  final interfaces = List<NetworkInterface?>.of(await NetworkInterface.list(
-      type: InternetAddressType.IPv4, includeLinkLocal: true));
+  final interfaces = List<NetworkInterface?>.of(await NetworkInterface.list(type: InternetAddressType.IPv4, includeLinkLocal: true));
   await Sentry.captureMessage(interfaces.toString());
 
   for (final interface in interfaces) {
     switch (interface?.name) {
       case 'en0':
+      case 'en1':
       case 'wlan0':
         ipAddress = interface?.addresses.first.address;
         break;
@@ -22,4 +22,3 @@ Future<String?> getLocalIpAddress(int port) async {
 
   return ipAddress;
 }
-
